aa0569d0f91c18bf635ad7ae6de2dd6fc550cff8
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-11-12 11:00:18 +0300

    Fixed token generation error

--- live_chat/web/api/users/schemas.py
+++ live_chat/web/api/users/schemas.py
@@ -1,16 +1,25 @@
 import uuid
 
 from fastapi_users import BaseUserManager, UUIDIDMixin, schemas
+from fastapi_users_db_sqlalchemy import UUID_ID
 
 from live_chat.db.models.chat import User  # type: ignore[attr-defined]
 from live_chat.settings import settings
 
 
-class UserRead(schemas.BaseUser[uuid.UUID]):
+class BaseUserSchema:
+    """Base user schemas."""
+
+    first_name: str
+    last_name: str
+    username: str
+
+
+class UserRead(BaseUserSchema, schemas.BaseUser[UUID_ID]):
     """Represents a read command for a user."""
 
 
-class UserCreate(schemas.BaseUserCreate):
+class UserCreate(BaseUserSchema, schemas.BaseUserCreate):
     """Represents a create command for a user."""
 
 
