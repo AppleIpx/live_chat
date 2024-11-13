bbc75f1e210add614a577731b7e89134461dd9a0
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-11-13 21:06:13 +0300

    added endpoint to get all users

--- live_chat/web/api/users/views.py
+++ live_chat/web/api/users/views.py
@@ -1,7 +1,16 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from live_chat.web.api.users.schemas import UserCreate, UserRead, UserUpdate
+from live_chat.db.models.chat import User  # type: ignore[attr-defined]
+from live_chat.db.utils import get_async_session
+from live_chat.web.api.users.schemas import (
+    ListUserSchema,
+    UserCreate,
+    UserRead,
+    UserUpdate,
+)
+from live_chat.web.api.users.utils.get_list_users import get_all_users, transformation
 from live_chat.web.api.users.utils.utils import api_users, auth_jwt
 
 http_bearer = HTTPBearer(auto_error=False)
@@ -35,3 +44,18 @@ router.include_router(
     prefix="/auth/jwt",
     tags=["auth"],
 )
+
+
+@router.get(
+    "/users",
+    tags=["users"],
+    summary="Create a direct chat",
+    response_model=ListUserSchema,
+)
+async def get_users(
+    db_session: AsyncSession = Depends(get_async_session),
+) -> ListUserSchema:
+    """Gets a list of all users."""
+    users: list[User] = await get_all_users(db_session)
+    users_data = transformation(users)
+    return ListUserSchema(users=users_data)
