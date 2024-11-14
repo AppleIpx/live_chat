a4fa20d6a1b7545bb0ad12ea63444338caa6debd
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-11-14 10:00:56 +0300

    removed extra code

--- live_chat/web/api/chat/views.py
+++ live_chat/web/api/chat/views.py
@@ -8,11 +8,10 @@ from live_chat.web.api.chat.schemas import (
     CreateDirectChatSchema,
     DisplayChatSchema,
     GetChatsSchema,
-    GetDetailChatSchema,
 )
 from live_chat.web.api.chat.utils.check_direct_chat_exists import direct_chat_exists
 from live_chat.web.api.chat.utils.create_direct_chat import create_direct_chat
-from live_chat.web.api.chat.utils.get_users_chats import get_user_chats
+from live_chat.web.api.chat.utils.get_users_chats import get_user_chats, transformation
 from live_chat.web.api.users.utils.check_user_auth import get_current_auth_user
 from live_chat.web.api.users.utils.get_user_by_id import get_user_by_id
 from live_chat.web.api.users.utils.utils import current_active_user
@@ -90,16 +89,7 @@ async def get_list_chats_view(
 ) -> GetChatsSchema:
     """Getting chats to which a user has been added."""
     chats: list[Chat] = await get_user_chats(db_session, current_user=current_user)
-    chats_data = [
-        GetDetailChatSchema(
-            chat_id=chat.id,
-            chat_type=chat.chat_type,
-            created_at=chat.created_at,
-            updated_at=chat.updated_at,
-            users=chat.users,
-        )
-        for chat in chats
-    ]
+    chats_data = transformation(chats)
 
     return GetChatsSchema(
         chats=chats_data,
