0bbaf98e6a72c989ba983437c6a6f56fa9a43d78
yuriy.belotserkovskiy <yuriy.belotserkovskiy@redcollar.ru>
2024-11-18 11:09:37 +0300

    Delete unnecessary code

--- live_chat/web/api/echo/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Echo API."""
-
-from live_chat.web.api.echo.views import router
-
-__all__ = ["router"]
--- live_chat/web/api/redis/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Redis API."""
-
-from live_chat.web.api.redis.views import router
-
-__all__ = ["router"]
