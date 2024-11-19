e921c4440bb850e82074aaee32543c0bd90a4be7
yuriy.belotserkovskiy <yuriy.belotserkovskiy@redcollar.ru>
2024-11-19 15:55:10 +0300

    Fix frontend

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -65,6 +65,7 @@ export default {
           email: this.email,
           first_name: this.first_name,
           last_name: this.last_name,
+          user_image: null,
         });
 
         if (response.status === 201) {
