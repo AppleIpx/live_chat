fab4c95f4a91d63af30d4eb4bd1dc4bc51f9354a
yuriy.belotserkovskiy <yuriy.belotserkovskiy@redcollar.ru>
2024-12-26 15:52:26 +0300

    Bugfix when delete user

--- live_chat/web/admin/models/users_admin.py
+++ live_chat/web/admin/models/users_admin.py
@@ -12,7 +12,7 @@ from wtforms.fields.simple import PasswordField, StringField
 from wtforms.validators import DataRequired, Email
 
 from live_chat.db.models.chat import User  # type: ignore[attr-defined]
-from live_chat.web.admin.utils import custom_object_identifier_values
+from live_chat.web.admin.utils import CustomQuery, custom_object_identifier_values
 from live_chat.web.admin.utils.transformation import transformation_new_user_admin
 
 
@@ -38,24 +38,24 @@ class UserAdmin(ModelView, model=User):
         User.username,
         User.first_name,
         User.last_name,
+        User.email,
         User.last_online,
         User.is_superuser,
+        User.is_deleted,
     )
     column_details_list = (
         *column_list,
-        User.email,
         User.user_image,
         User.hashed_password,
         User.created_at,
         User.is_active,
-        User.is_deleted,
         User.is_verified,
     )
     form = UserForm
     column_searchable_list = (User.username, User.email)
     column_sortable_list = (User.username, User.is_superuser)
     can_create = True
-    can_edit = True
+    can_edit = False
     can_delete = True
     can_view_details = True
     name = "User"
@@ -69,6 +69,10 @@ class UserAdmin(ModelView, model=User):
         """Function to add a new user through the admin panel."""
         data = await transformation_new_user_admin(data=data)
         return await super().insert_model(request, data)
+
+    async def delete_model(self, request: Request, pk: Any) -> None:
+        """Function to mark user is_deleted flag through the admin panel."""
+        await CustomQuery(self).delete(pk, request)
 
     def _stmt_by_identifier(self, identifier: str) -> Select[Any]:
         stmt: Select[Any] = select(self.model)
--- live_chat/web/admin/utils/__init__.py
+++ live_chat/web/admin/utils/__init__.py
@@ -1,3 +1,4 @@
+from live_chat.web.admin.utils.custom_query import CustomQuery
 from live_chat.web.admin.utils.identifier_utils import custom_object_identifier_values
 
-__all__ = ("custom_object_identifier_values",)
+__all__ = ("custom_object_identifier_values", "CustomQuery")
